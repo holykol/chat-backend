0315d0389b668de2712ebf431d3bae7d84031f2d
Timofey <1079849-holykol@users.noreply.gitlab.com>
2019-07-11 11:35:56 +1100

    Setup schema

--- lib/chat_web/schema.ex
+++ lib/chat_web/schema.ex
@@ -1,40 +1,23 @@
-# defmodule ChatWeb.Schema do
-#   use Absinthe.Schema
-#   import_types ChatWeb.Schema.ContentTypes
-
-#   alias ChatWeb.Resolvers
-
-#   query do
-
-#     # @desc "Get all chat rooms"
-#     # field :allchats, list_of(:chat) do
-#       # resolve []
-#     # end
-
-#     field :hello, :string do
-#     	resolve "world"
-#     end
-
-#   end
-
-# end
-
 defmodule ChatWeb.Schema do
   use Absinthe.Schema
 
- 	# Example data
-  @items [
-    %{id: "foo", title: "Foo"},
-    %{id: "bar", title: "Bar"}
-  ]
-
 	object :chat do
-	  field :id, :id
-	  field :title, :string
+	  field :id, non_null(:id)
+	  field :title, non_null(:string)
+
 	  field :creator, :user do
-	  	resolve fn _, _ ->
-	  		{:ok, %{id: "1", username: "deadrime"}}
-	  	end
+	  	resolve fn _, _ -> {:error, "not implemented"} end
+	  end
+
+	  field :members, list_of(:user) do
+	  	resolve fn _, _ -> {:error, "not implemented"} end
+	  end
+
+	  field :messages, list_of(:message) do
+	  	arg :last_id, :id
+	  	arg :limit, :integer
+
+	  	resolve fn _, _ -> {:error, "not implemented"} end
 	  end
 	end
 
@@ -44,26 +27,33 @@ defmodule ChatWeb.Schema do
 	end
 
 	object :message do
-		field :id, :id
-		field :text, :string
-		field :from, :user
-		field :chat_id, :id
-		field :chat, :chat
+		field :id, non_null(:id)
+		field :text, non_null(:string)
+		field :from, non_null(:user)
+		field :chat_id, non_null(:id)
+		field :chat, non_null(:chat)
 		field :created_at, :integer
 	end
 
   query do
+  	field :token, :string do
+  		arg :username, non_null(:string)
+  		arg :password, non_null(:string)
+
+	  	resolve fn _, _ -> {:error, "not implemented"} end
+	  end
+
+  	field :me, :user do
+  		resolve fn _, _ -> {:error, "not implemented"} end
+  	end
+
     field :chat, :chat do
       arg :id, non_null(:id)
-      resolve fn %{id: item_id}, _ ->
-        {:ok, Enum.find(@items, &(&1.id == item_id))}
-      end
+      resolve fn _, _ -> {:error, "not implemented"} end
     end
 
-    field :all_chats, list_of(:chat) do
-    	resolve fn _, _ ->
-    		{:ok, @items}
-	    end
+    field :my_chats, list_of(:chat) do
+    	resolve fn _, _ -> {:error, "not implemented"} end
 	  end
 
   end
