5e59184d8d25601ae71e80738b4764b0fe0e5e36
Timofey <1079849-holykol@users.noreply.gitlab.com>
2019-07-16 00:00:10 +1100

    Implement user login

--- lib/chat_web/resolvers/users.ex
+++ lib/chat_web/resolvers/users.ex
@@ -1,22 +1,32 @@
 defmodule ChatWeb.Resolvers.Users do
 	alias Chat.{Repo, User}
 
-	def login(_parent, _args, _resolution) do
-		{:error, "not_implemented"}
+	def login(_parent, %{username: username, password: password} = args, _resolution) do
+		user = Repo.get_by(User, username: username)
+
+		case Bcrypt.verify_pass(password, user.password) do
+			true -> {:ok, issue_token(user.id)}
+			false -> {:error, "wrong password"}
+		end
 	end
 
 	def register(_parent, args, _resolution) do
 		password = Bcrypt.hash_pwd_salt(args.password)
 
-		# TODO beautiful errors
+		# TODO do sth about errors
+		# https://hexdocs.pm/ecto/Ecto.Repo.html#c:insert/2-examples
 		{:ok, user} = Repo.insert(%User{
 			username: args.username,
 			name: args.username,
 			password: password
 		})
 
-		token = JsonWebToken.sign(%{id: user.id}, %{key: ChatWeb.Endpoint.config(:secret_key_base)})
+		token = issue_token(user.id)
 
 		{:ok, token}
+	end
+
+	defp issue_token(id) do
+		JsonWebToken.sign(%{id: id}, %{key: ChatWeb.Endpoint.config(:secret_key_base)})
 	end
 end
